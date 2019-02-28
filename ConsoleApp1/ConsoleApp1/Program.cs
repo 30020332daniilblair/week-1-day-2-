75e1c7f93e6507368cf26e4cc441802231b3567b
Daniil Blair <30020332@student.toiohomai.ac.nz>
2019-02-28 13:51:47 +1300

    comment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("What is your first name" ); 
+            string fname = Console.ReadLine();
+            Console.WriteLine("What is your last name");
+            string Lname = Console.ReadLine();
+            Console.WriteLine("{0} {1}", fname, Lname);
+            Console.ReadLine(); */
+
+            Console.WriteLine("Please write your name");
+            string fname = Console.ReadLine();
+            Console.WriteLine("please write your last name");
+            string lname = Console.ReadLine();
+            Console.WriteLine("please write your adress");
+            string adress = Console.ReadLine();
+            Console.WriteLine("what is your age");
+            string age = Console.ReadLine();
+            Console.WriteLine("what is your email addy");
+            string addy = Console.ReadLine();
+            Console.WriteLine("{0} {1} {2} {3} {4}", fname, lname, adress, age, addy);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
